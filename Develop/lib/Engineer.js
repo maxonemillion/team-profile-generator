6c79ea1cd3036a08fb334ea9b5d608801f5a9199
maxonemillion <max.daniel@utah.edu>
2020-10-01 22:31:25 -0600

    initial commit

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,18 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee")
+
+class Engineer extends Employee {
+    constructor(name, id, email, github) {
+        this.github = github;
+    }
+    
+    getRole () {
+        return this.role; // or "engineer"?
+    }
+
+    getGithub () {
+        return this.name;
+    }
+}
+
+module.exports = Engineer;
--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,1 +1,18 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee")
+
+class Intern extends Employee {
+    constructor(name, id, email, school) {
+        this.school = school;
+    }
+    
+    getRole () {
+        return this.role; // or "intern"?
+    }
+
+    getSchool () {
+        return this.school;
+    }
+}
+
+module.exports = Intern;
